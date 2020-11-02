be3222ad3e2e5d2d68a1966cbac541a78ac1ecd1
Cristian Tavarez <cristian.javier.11@hotmail.com>
2020-11-01 20:22:36 -0400

    display info from object on array

--- src/index.js
+++ src/index.js
@@ -2,31 +2,44 @@ import React from "react";
 import ReactDom from "react-dom"; // index.js is an entry-point for the application.
 // CSS
 import "./index.css";
-const firstBook = {
-  img:
-    "https://lh3.googleusercontent.com/proxy/gnNXVcC8jFknWkOjpQhbIk-Ymk-CJM6lDbDP7QoUsbF-e9iaqeXLtMqqS-78XZaqgSuHRXp6geEe3sfLuoBtRdEHHyeYwHv688C15Hk26UC8brOLyU5J",
-  title: "Book Title",
-  author: "Book Author",
-};
-const secondBook = {
-  img:
-    "https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/22739/optimized_large_thumb_children-stories-book-cover-541__1_.jpg",
-  title: "Book Title2",
-  author: "Book Author2",
-};
+const books = [
+  {
+    id: 1,
+    img:
+      "https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/22739/optimized_large_thumb_children-stories-book-cover-541__1_.jpg",
+    title: "Book Title",
+    author: "Book Author",
+  },
+  {
+    id: 2,
+    img:
+      "https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/22739/optimized_large_thumb_children-stories-book-cover-541__1_.jpg",
+    title: "Book Title2",
+    author: "Book Author2",
+  },
+  {
+    id: 3,
+    img:
+      "https://img0-placeit-net.s3-accelerate.amazonaws.com/uploads/stage/stage_image/22739/optimized_large_thumb_children-stories-book-cover-541__1_.jpg",
+    title: "Book Title3",
+    author: "Book Author3",
+  },
+];
+
 function BookList() {
   // First letter on function name must be capitalized to count as a React Component.
   return (
     <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+      {books.map((book) => {
+        return <Book key={book.id} book={book} />;
+      })}
     </section>
   );
   // Returns JSX
 }
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
